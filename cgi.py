7f1f16a3f1f91081ec97c3bb07432de3e32f2dfd
PARJINDER1715 <singhparjinder1843@gmail.com>
2025-10-04 13:34:27 +0530

    cgi.py

---
+++ cgi.py
@@ -0,0 +1,7 @@
+# Example helper file
+def parse_header(line):
+    """
+    Simple parser that returns the line and an empty dictionary.
+    You can extend this for CGI headers or other parsing.
+    """
+    return line, {}
